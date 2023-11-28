f47f7af95a5b7da922fa34cc581b55f71a13aef2
MindYume <youwatchdream@gmail.com>
2023-11-28 06:13:13 +0300

    Added EnergyEffect

---
+++ Assets/Scripts/EnergyEffect.cs
@@ -0,0 +1,68 @@
+/*
+MIT License
+
+Copyright (c) 2023 Viktor Grachev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using UnityEngine;
+
+public class EneryEffect : MonoBehaviour
+{
+    [SerializeField] private Color _color;
+    private Transform[] _circleTransforms;
+    private SpriteRenderer[] _circleSprites;
+
+    private float _energyValue = 1;
+
+    void Start()
+    {
+        _circleSprites = GetComponentsInChildren<SpriteRenderer>();
+        _circleTransforms = new Transform[3];
+        _circleTransforms[0] = transform.Find("Circle1");
+        _circleTransforms[1] = transform.Find("Circle2");
+        _circleTransforms[2] = transform.Find("Circle3");
+    }
+    void Update()
+    {
+        for (int i = 0; i < _circleTransforms.Length; i++)
+        {
+            _circleSprites[i].color = new Color(_color.r, _color.g, _color.b, (1 - _circleTransforms[i].localScale.x) * _energyValue);
+            
+            _circleTransforms[i].localScale -= Vector3.one * Time.deltaTime * 4f;
+
+            if (_circleTransforms[i].localScale.x <= 0)
+            {
+                _circleTransforms[i].localScale += Vector3.one;
+                _circleSprites[i].color = new Color(_color.r, _color.g, _color.b, 0);
+                
+                /* if (playSound)
+                {
+                    _generalSingleton.PlaySound("wave", (-50 + _circles[i].Scale.x  * _energyValue * 60), 1);
+                } */
+            }
+        }
+    }
+
+    public float EnergyValue
+    {
+        set {_energyValue = value;}
+    }
+}
